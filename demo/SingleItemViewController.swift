072519c5886248fbcd1897a7d249464a12a340f8
dusan vesic <dusan@MacBookPro.local>
2017-02-06 22:24:22 +0100

    refactor SingleItemViewController

--- demo/SingleItemViewController.swift
+++ demo/SingleItemViewController.swift
@@ -1,33 +1,26 @@
-//
-//  SingleItemViewController.swift
-//  demo
-//
-//  Created by dusan vesic on 06/02/2017.
-//  Copyright © 2017 dusan vesic. All rights reserved.
-//
-
 import UIKit
 
 class SingleItemViewController: UIViewController {
 
+    // item placeholder
     var item: Item?
     
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var itemLabel: UILabel!
-
     @IBOutlet weak var itemImage: UIImageView!
     
     override func viewDidLoad() {
         
-        self.title = "hello"
+        self.title = "Single Item"
         
         self.itemLabel.text = item?.title
         self.descriptionLabel.text = item?.description
 
-        // async
+        // async image load
         self.loadImage()
         
+        // sync image load
         /*
         let url = URL(string: (self.item?.image)!)
         let data = try? Data(contentsOf: url!)
@@ -36,24 +29,30 @@ class SingleItemViewController: UIViewController {
         
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
+    
         self.spinner.isHidden = false
         self.spinner.startAnimating()
+    
     }
+    
     func loadImage() {
+    
         let url = URL(string: (self.item?.image)!)
         let req = URLRequest(url: url!)
+        
         URLSession.shared.dataTask(with: req) {
             (data, response, error) in
+            // update UI from background
             DispatchQueue.main.async {
                 self.itemImage.image = UIImage(data: data!)
                 self.spinner.stopAnimating()
                 self.spinner.isHidden = true
             }
         }.resume()
+        
     }
-
+    
 }
